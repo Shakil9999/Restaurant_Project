1ad2619ac448f123f675d5dd0e38f3fecd401ce8
shakil <shakilsabeer@gmail.com>
2025-12-03 18:18:41 +0600

    Updated API base URL to live Render server

--- sk-kitchen-restaurant-server-site/index.js
+++ sk-kitchen-restaurant-server-site/index.js
@@ -10,11 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:5173', "https://restaurant-project-5q8t.vercel.app"],
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
